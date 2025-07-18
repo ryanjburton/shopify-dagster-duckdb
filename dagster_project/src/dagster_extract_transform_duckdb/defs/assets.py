0a93c13fe817a390dd6e16e22cdea038df591d32
Ryan Burton <ryan_burton@ryans-mbp.lan>
2025-07-18 13:32:49 -0700

    update dagster project with duckdb and shopify api to extract and load product data

--- dagster_project/src/dagster_extract_transform_duckdb/defs/assets.py
+++ dagster_project/src/dagster_extract_transform_duckdb/defs/assets.py
@@ -1,21 +1,114 @@
 import pandas as pd
-
 import dagster as dg
-
-sample_data_file = "src/dagster_extract_transform_duckdb/defs/data/sample_data.csv"
-processed_data_file = "src/dagster_extract_transform_duckdb/defs/data/processed_data.csv"
-
+import numpy as num
+import duckdb as db
+import shopify as shop
+import requests as req
+import os
+from datetime import datetime
+from dotenv import load_dotenv
 
 @dg.asset
-def processed_data():
-    ## Read data from the CSV
-    df = pd.read_csv(sample_data_file)
+def establish_duck_db_conn():
+    ## Adjust current working directory to point to Database file folder location.
+    os.chdir('..')
+    
+    subfolder_name = "duckdb_database"
 
-    ## Add an age_group column based on the value of age
-    df["age_group"] = pd.cut(
-        df["age"], bins=[0, 30, 40, 100], labels=["Young", "Middle", "Senior"]
-    )
+    os.chdir(subfolder_name)
 
-    ## Save processed data
-    df.to_csv(processed_data_file, index=False)
-    return "Data loaded successfully"
+    ## Create a connection to a persisted DuckDB database, setup for local execution and storage.
+    con = db.connect("database.db")
+    
+    ## Pull test data related to table metadata in database to ensure proper connection.
+    tables_info_df = con.execute("SELECT * FROM duckdb_tables;").fetchdf()
+    tables_info_df = pd.DataFrame(tables_info_df)
+    
+    ## Test validity of non-NULL database connection.
+    def test_dataframe_not_empty(df):
+        return not df.empty
+
+    test_df_availability_status = test_dataframe_not_empty(tables_info_df)
+
+    if test_df_availability_status != True:
+        raise dg.Failure(
+            f"Database Connection issue: Null Data"
+        )
+
+    return "Database connection successful"
+
+
+@dg.asset()
+def establish_shopify_api_conn():
+    ## Load variable from .env file
+    load_dotenv()
+
+    ## Specify Shopify store API metadata for conn; for testing purposes a dummy dev store has been setup with the following properties.
+    shop_url = os.getenv("SHOPIFY_API_URL")
+    api_version = os.getenv("SHOPIFY_API_VERSION")
+    api_key = os.getenv("SHOPIFY_API_KEY")
+    # Create a Shopify session
+    session = shop.Session(shop_url, api_version, api_key)
+
+    # Activate the session
+    shop.ShopifyResource.activate_session(session)
+
+    # Interact with the Shopify API to extract the store name and verify successful connection.
+    shop_id = shop.Shop.current()
+    shop_name = shop_id.name
+
+    ## Note that the below business name is just a testing instantiation and would require updating for production.
+    if shop_name != 'No Name Business 123':
+        raise dg.Failure(
+            f"Shopify API connection failure"
+        )
+
+    return "Shopify API connection success"
+
+
+@dg.asset(deps=[establish_duck_db_conn, establish_shopify_api_conn])
+def extract_load_shopify_orders_data():
+    ## Load variable from .env file
+    load_dotenv()
+
+    ## Re-establish Shopify API connection for Products data extraction. Utilize connection via Python requests package for ease of API json extraction.
+    shop_url = os.getenv("SHOPIFY_API_URL")
+    api_version = os.getenv("SHOPIFY_API_VERSION")
+    api_key = os.getenv("SHOPIFY_API_KEY")
+
+    headers = {
+        "X-Shopify-Access-Token": api_key,
+        "Content-Type": "application/json"
+    }
+
+    ## Fetching products data
+    products_url = f"https://{shop_url}/admin/api/{api_version}/products.json"
+    response = req.get(products_url, headers=headers)
+    products_data = response.json().get("products", [])
+    products_df = pd.DataFrame(products_data)
+
+    ## Add a snapshot data for data versioning control downstream
+    products_df['snapshot_date'] = datetime.now()
+
+    ## Re-establish DuckDB database connection for data loading and downstream transformation.
+    os.chdir('..')
+    subfolder_name = "duckdb_database"
+    os.chdir(subfolder_name)
+
+    con = db.connect("database.db")
+
+    ## Ensure the creation of an entities schema in the persisted DuckDB database to store data as a 1:1 relationship with upstream source.
+    entities_schema_name = "entities"
+    con.execute("CREATE SCHEMA IF NOT EXISTS entities_schema_name;")
+
+    ## Ensure the creation of an entities data table for Shopify Products in the persisted DuckDB database with a 1:1 relationship with the upstream Shopify Orders API. 
+    con.execute("CREATE TABLE IF NOT EXISTS entities.shopify_products AS SELECT * FROM products_df;")
+
+    try:
+        con.execute("ALTER TABLE entities.shopify_products ADD PRIMARY KEY(id, snapshot_date);")
+    except db.duckdb.CatalogException:
+        pass
+
+    con.execute("INSERT OR REPLACE INTO entities.shopify_products SELECT * FROM products_df;")
+
+    return "Shopify - Products data successfully written to DuckDB database"
